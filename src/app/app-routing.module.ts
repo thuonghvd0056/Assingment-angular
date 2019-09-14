54728b2a6893b9a06c51865656f87b7f6c2aad67
thuonghvd0056 <thuonghvd00564@fpt.edu.vn>
2019-09-14 18:02:35 +0700

    lan2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {ListCoinComponent} from './coin/list-coin/list-coin.component';
+import {CreateCoinComponent} from './coin/create-coin/create-coin.component';
+import {CreateMarketComponent} from './market/create-market/create-market.component';
+import {ListMarketComponent} from './market/list-market/list-market.component';
 
 
 const routes: Routes = [
-  {path: 'list', component: ListCoinComponent}
+  {path: '', component: ListCoinComponent},
+  {path: 'createCoin', component: CreateCoinComponent},
+  {path: 'createMarket', component: CreateMarketComponent},
+  {path: 'listMarket', component: ListMarketComponent},
 ];
 
 @NgModule({
